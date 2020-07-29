15469994239b70170802fe9097e900b10cb1c28c
Kanstantsin Kulak <kkulak24@gmail.com>
2020-07-29 20:33:08 +0300

    feat: add linting

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,12 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 class App extends React.Component {
-    render() {
-        return <div>Hello {this.props.name}</div>;
-    }
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
 }
 
 const AppWithHot = hot(module)(App);
 
-var mountNode = document.getElementById('app');
+const mountNode = document.getElementById('app');
 ReactDOM.render(<AppWithHot name="Jane" />, mountNode);
