0bc56015650be1cf3eb54f87e8e8736d53375b61
Kanstantsin Kulak <kkulak24@gmail.com>
2020-08-30 17:34:04 +0300

    feat: change style

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import App from './components/app/app';
-import 'bootswatch/dist/darkly/bootstrap.min.css';
+import 'bootswatch/dist/materia/bootstrap.min.css';
 
 const appNode = document.getElementById('root');
 ReactDOM.render(<App />, appNode);
